fae7ecfa0129b031a4bba186d54f5017f2bb27fe
gabbycam98 <gabcamino1298@gmail.com>
2025-07-14 09:40:05 +0800

    feat: define additional path constants for project structure

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,10 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
+define('HANDLERS_PATH', realpath(BASE_PATH . '/handlers'));
+define('LAYOUTS_PATH', realpath(BASE_PATH . '/layouts'));
+define('TEMPLATES_PATH', realpath(BASE_PATH . '/components/templates'));
+define('STATICDATAS_PATH', realpath(BASE_PATH . '/staticDatas'));
+define('UTILS_PATH', realpath(BASE_PATH . '/utils'));
+
 
 chdir(BASE_PATH);
