f030dead3c9b9fe6e16d25e261428c98d1379bd8
gabbycam98 <gabcamino1298@gmail.com>
2025-07-14 10:38:19 +0800

    feat: converted header and footer as components

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
-
+<?php
+require_once __DIR__ . '/init.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +12,8 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/header.css">
@@ -17,29 +21,23 @@
 
 
 </head>
+
 <body>
 
     <!-- header section -->
-    <header class="header">
-        <div class="left-section">
-            <a href="../../index.php">
-                <img src="../../assets/img/travelez-green.png" alt="travelEZ logo">
-            </a>
-        </div>
-        <div class="middle-section">
-        </div>
-        <div class="right-section">
-            <button class="nav-header">About Us</button>
-        </div>
-    </header>
+    <?php
+    include 'components/templates/header.component.php';
+    ?>
 
-    
     <main>
         <section class="landing-page">
             <div class="description-container">
                 <div class="travelez-description">
                     <h1>Welcome to TravelEZ!</h1>
-                    <p class="description">TravelEZ is a user-friendly website that provides a comprehensive timetable of bus routes across the Philippines. Whether you're planning a trip from Manila to North Luzon or other key destinations, TravelEZ helps you explore available routes offered by various bus companies!</p>
+                    <p class="description">TravelEZ is a user-friendly website that provides a comprehensive timetable
+                        of bus routes across the Philippines. Whether you're planning a trip from Manila to North Luzon
+                        or other key destinations, TravelEZ helps you explore available routes offered by various bus
+                        companies!</p>
                 </div>
                 <div class="button-container">
                     <a href="page/timetable/index.php">
@@ -47,24 +45,17 @@
                     </a>
                 </div>
             </div>
-            
-            
+
+
         </section>
 
     </main>
 
-    <footer class="footer">
-        <div class="footer-container">
-            <div class="footer-image">
-                <img src="../assets/img/travelez-green.png" alt="TravelEZ Logo">
-            </div>
-            <div class="copyright-info">
-                <p>© 2025 TravelEZ Philippines, All Rights Reserved.</p>
-            </div>
-        </div>
-    </footer>
+    <!-- footer section -->
+    <?php
+    include 'components/templates/footer.component.php';
+    ?>
 
 </body>
-</html>
 
-        
+</html>
